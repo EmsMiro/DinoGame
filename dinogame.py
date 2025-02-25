6326db6d18bf7d5518f55380bd463e9a3fbdfbb3
EmsMiro <ednirlicosta@gmail.com>
2025-02-25 15:09:01 -0300

    mecanica dos coletáveis e score finalizada e corrigida

--- dinogame.py
+++ dinogame.py
@@ -1,14 +1,14 @@
 import pgzrun
 import random
-from pygame import Rect # Importar somente Rect do pygame
+from pygame import Rect
 
-# Configurações da tela
+# config da tela
 WIDTH = 600
 HEIGHT = 400
 background_x = 0
 velocity_camera = 2
- 
-# configurações do personagem
+
+# config do personagem
 player = Actor('dino')
 player.x = 100
 player.y = HEIGHT - 40
@@ -23,61 +23,61 @@ plataforms = []
 PLATAFORM_HEIGHT = 3
 PLATAFORM_MIN_WIDTH = 30
 PLATAFORM_MAX_WIDTH = 80
-MIN_VERTICAL_DISTANCE = 30
-MIN_HORIZONTAL_DISTANCE = 50
-
 PLATFORM_MIN_Y = HEIGHT - 100
 PLATFORM_MAX_Y = HEIGHT // 2
 
-# variáveis para mecânica do pulo
+# variáveis para a mecânica do pulo
 gravity = 0.5
 velocity_y = 0
 jumping = False
 double_jump = False
-score = 0
-eggs = []
+score = 0  
+eggs = []  # array p/ armazenar os ovos
+available_eggs = []  # array p/ ovos que ainda estão disponíveis
+collected_eggs = []  # array p/ rastrear os ovos coletados
 
-#função para criar as plataformas no mapa
+# função p/ criar as plataformas no mapa
 def create_plataforms():
-    global plataforms, eggs
+    global plataforms, eggs, available_eggs
     plataforms = []
     eggs = []
+    available_eggs = []  # reiniciar a lista de ovos disponíveis
     num_plataforms = 2
 
     for _ in range(num_plataforms):
         width = random.randint(PLATAFORM_MIN_WIDTH, PLATAFORM_MAX_WIDTH)
         x = random.randint(100, WIDTH - width)
 
-        #lógica p/ garantir que as plataformas não se sobreponham
         valid_position = False
         while not valid_position:
             y = random.randint(PLATFORM_MAX_Y, PLATFORM_MIN_Y)
             valid_position = True
             for plataform, _ in plataforms:
-                if abs(plataform.y - y) < MIN_VERTICAL_DISTANCE:
+                if abs(plataform.y - y) < 30:
                     valid_position = False
                     break
-
-                if abs(plataform.x - x) < (plataform.width + MIN_HORIZONTAL_DISTANCE):
+                if abs(plataform.x - x) < (plataform.width + 50):
                     valid_position = False
                     break
 
         plataform = Rect(x, y, width, PLATAFORM_HEIGHT)
 
+        # adicionar um ovo aleatoriamente
         egg = None
         if random.random() < 0.5:
             egg_x = x + width // 2
             egg_y = y - 20
             egg = Actor('dino_egg', (egg_x, egg_y))
-            eggs.append((egg, plataform))
+            eggs.append(egg)  
+            available_eggs.append(egg)  # lista de eggs disponíveis a serem alcançados
 
         plataforms.append((plataform, egg))
 
 create_plataforms()
 
-#funçao de atualização da tela do game
+# função de atualização da tela do game
 def update(dt):
-    global background_x, current_image_index, animation_timer, velocity_y, jumping, double_jump
+    global background_x, current_image_index, animation_timer, velocity_y, jumping, double_jump, score, collected_eggs
     background_x -= velocity_camera
 
     if background_x <= -WIDTH:
@@ -87,7 +87,6 @@ def update(dt):
         velocity_y += gravity
         player.y += velocity_y
 
-        # verificação se o jogador atingiu o solo
         on_ground = False
         for plataform, _ in plataforms:
             if player.colliderect(plataform) and velocity_y >= 0:
@@ -97,12 +96,11 @@ def update(dt):
                 double_jump = False
                 break
             
-        # verifica se o player caiu abaixo do chão
         if player.y >= HEIGHT - player.height // 2:
             player.y = HEIGHT - player.height // 2
             jumping = False
 
-    # moveimentação da câmera do game
+    # atualiza as plataformas e ovos
     for i in range(len(plataforms)):
         plataform, egg = plataforms[i]
         plataform.x -= velocity_camera
@@ -110,7 +108,6 @@ def update(dt):
         if egg:
             egg.x -= velocity_camera
             
-        # verificação para saber se a plataforma saiu da tela
         if plataform.right < 0:
             width = random.randint(PLATAFORM_MIN_WIDTH, PLATAFORM_MAX_WIDTH)
             new_x = WIDTH + random.randint(50, 200)
@@ -122,15 +119,20 @@ def update(dt):
                 egg_x = new_x + width // 2
                 egg_y = new_y - 15
                 new_egg = Actor('dino_egg', (egg_x, egg_y))
+                available_eggs.append(new_egg) 
 
             plataforms[i] = (new_plataform, new_egg)
 
-    for i, (egg, plataform) in enumerate(eggs):
+    # verificação de colisão com os coletáveis
+    for i in range(len(available_eggs) - 1, -1, -1):  # percorrer de trás para frente
+        egg = available_eggs[i]
         if player.colliderect(egg):
-            eggs.pop(i)
-            score += 1
+            score += 1  # atualiza o score
+            collected_eggs.append(egg)  # adiciona à lista de ovos coletados
+            available_eggs.pop(i)  # remover o ovo já coletado
+            print(f'Ovo coletado! Score: {score}')  # debug
 
-    # atualiza os sprites de animação do personagem
+    # atualiza a animação do player
     animation_timer += dt
     if animation_timer > animation_speed:
         current_image_index = (current_image_index + 1) % len(player_images)
@@ -138,7 +140,7 @@ def update(dt):
 
     player.image = player_images[current_image_index]
 
-#função de escuta do teclado do player
+# função de escuta do teclado do player
 def on_key_down(key):
     global jumping, velocity_y, double_jump
     if key == keys.SPACE:
@@ -149,7 +151,7 @@ def on_key_down(key):
             double_jump = True
             velocity_y = -10
 
-#função que cria os elementos na tela
+# função que desenha os elementos na tela
 def draw():
     screen.fill((255, 255, 255))
 
@@ -157,10 +159,12 @@ def draw():
     screen.blit("ground", (background_x, HEIGHT - ground_height))
     screen.blit("ground", (background_x + WIDTH, HEIGHT - ground_height))
 
-    for plataform, egg in plataforms:
+    for plataform, _ in plataforms:
         screen.draw.filled_rect(plataform, (83, 83, 83))
-        if egg:
-            egg.draw()
+
+    # desenha os ovos que não foram coletados
+    for egg in available_eggs:
+        egg.draw()
 
     player.draw()
     screen.draw.text(f'Score: {score}', (10, 10), color='black')
