8867561e2b1d18302361a874d7f2260b82cd9f5b
EmsMiro <ednirlicosta@gmail.com>
2025-02-25 20:20:20 -0300

    adicionando tela incial com botão start finalizado

--- dinogame.py
+++ dinogame.py
@@ -53,6 +53,8 @@ birds = []
 
 # variáveis de estado do jogo
 game_over = False
+game_started = False
+start_button = Rect(230, 150, 140, 50) 
 
 def create_plataforms():
     global plataforms, eggs, available_eggs
@@ -109,6 +111,9 @@ create_plataforms()
 def update(dt):
     global background_x, current_image_index, animation_timer, velocity_y, jumping, double_jump, score, collected_eggs
     global bird_animation_timer, bird_animation_index, game_over
+
+    if not game_started:  # verifca se o jogo não começou, não atualiza
+        return 
 
     if game_over:
         return 
@@ -185,7 +190,6 @@ def update(dt):
         if player.colliderect(cactus):
             game_over = True
 
-   
     cacti[:] = [cactus for cactus in cacti if cactus.right > 0]
 
     # atualiza os pássaros
@@ -195,12 +199,12 @@ def update(dt):
     for bird in birds:
         bird.x -= 3  
         if player.colliderect(bird):
-            game_over = True  # Colisão com pássaros
+            game_over = True  # colisão com pássaros
 
     # remover pássaros fora da tela
     birds[:] = [bird for bird in birds if bird.right > 0]
 
-    # aualiza a animação do pássaro
+    # atualiza a animação do pássaro
     bird_animation_timer += dt
     if bird_animation_timer > bird_animation_speed:
         bird_animation_index = (bird_animation_index + 1) % len(bird_images)
@@ -215,13 +219,27 @@ def on_key_down(key):
     if key == keys.SPACE:
         if not jumping:
             jumping = True
-            velocity_y = -12
+            velocity_y = -13
         elif not double_jump:
             double_jump = True
             velocity_y = -10
 
+# função para escutar cliques do mouse
+def on_mouse_down(pos):
+    global game_started
+    if not game_started and start_button.collidepoint(pos):
+        game_started = True  
+
 # função que desenha os elementos na tela
 def draw():
+    if not game_started:  # esse primeiro if verifica se o jogo não começou, e desenha a tela inicial
+        screen.blit("telainicial", (0, 0))         
+        screen.draw.text("DINO GAME", center=(WIDTH // 2, start_button.y - 30), fontname="gameplay", color='#535353', fontsize=50)        
+        screen.draw.rect(start_button, (0, 0, 0)) 
+        screen.draw.text("Start", center=(start_button.x + start_button.width // 2, start_button.y + start_button.height // 2), color='#535353', fontname="gameplay", fontsize=30)
+        return
+
+    # desenha a tela do game em si
     screen.fill((255, 255, 255))
     ground_height = images.ground.get_height()
     screen.blit("ground", (background_x, HEIGHT - ground_height))
